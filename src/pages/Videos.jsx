4a92d569e57b6f3e72e94aa045de75e0e82f97be
ANTO <dosaeun@gmail.com>
2023-03-03 01:11:51 +0900

    test4

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from "react-router-dom";
 import VideoCard from '../components/VideoCard';
-// import axios from 'axios';
-import { search } from '../api/youtubeApi';
+import FakeYoutube from '../api/FakeYoutube';
+import Youtube from '../api/Youtube';
+// import Search from '../components/Search';
+
 
 export default function Videos() {
     const { keyword } = useParams();
 
     const {isLoading, error, data:videos} = useQuery(
-        [ 'videos', keyword ], () => search(keyword));
+        [ 'videos', keyword ], () => {
+            const youtube = new Youtube();
+            return youtube.search(keyword);
+        });
+        
     return (
         <div>
             {isLoading && <p>Loading..</p>}
