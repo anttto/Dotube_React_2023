b116d61f505337c88936648d746be00930e4c8de
Antovero <dosaeun@gmail.com>
2023-03-08 23:14:44 +0900

    modify..3

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,8 +1,8 @@
 import React, { createContext, useContext } from 'react';
-import FakeYoutube from '../api/FakeYoutube';
+// import FakeYoutube from '../api/FakeYoutube';
 import Youtube from '../api/Youtube';
 
-const youtube = new FakeYoutube();
+const youtube = new Youtube();
 
 export const YoutubeApiContext = createContext();
 
